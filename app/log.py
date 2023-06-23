6110f4843d68ee163076d94c5fb94bae3e84da0b
jaylco <77386599+jaylco@users.noreply.github.com>
2023-06-23 16:24:07 +0100

    app insights

--- app/log.py
+++ app/log.py
@@ -1,4 +1,9 @@
+from config import APPLICATIONINSIGHTS_CONNECTION_STRING
 import logging
 
 logging.basicConfig(format='{"timestamp":"%(asctime)s", "level":"%(levelname)s", "message":"%(message)s"}', level="INFO")
 logger = logging.getLogger()
+
+if APPLICATIONINSIGHTS_CONNECTION_STRING:
+    from opencensus.ext.azure.log_exporter import AzureLogHandler
+    logger.addHandler(AzureLogHandler())

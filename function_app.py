6d8df327a4d0841a17bbb933b52b2ffb72e6f481
jaylco <77386599+jaylco@users.noreply.github.com>
2023-07-03 11:34:04 +0100

    test functions

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ DUPLICATE_TOKEN_CRON = os.environ['DUPLICATE_TOKEN_CRON']
 REDUNDANT_TOKEN_CRON = os.environ['REDUNDANT_TOKEN_CRON']
 
 
-@app.timer_trigger(schedule=DUPLICATE_TOKEN_CRON, arg_name="myTimer", run_on_startup=False)
+@app.timer_trigger(schedule=DUPLICATE_TOKEN_CRON, arg_name="myTimer", run_on_startup=True)
 def DuplicateTokens(myTimer: func.TimerRequest) -> None:
     tokens.run_clear_duplicate_tokens()
 
@@ -15,3 +15,24 @@ def DuplicateTokens(myTimer: func.TimerRequest) -> None:
 @app.timer_trigger(schedule=REDUNDANT_TOKEN_CRON, arg_name="myTimer", run_on_startup=False)
 def RedundantTokens(myTimer: func.TimerRequest) -> None:
     tokens.run_clear_redundant_tokens()
+
+
+@app.route(route="HttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
+def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )

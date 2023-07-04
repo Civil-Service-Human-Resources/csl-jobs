f2f87af4e10e4e7a757e098d708c5813d23e59ad
jaylco <77386599+jaylco@users.noreply.github.com>
2023-07-04 15:49:29 +0100

    configurable run-on-startup settings

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,11 @@ export const DATABASE_ENABLE_DEBUG: boolean = JSON.parse(env.DATABASE_ENABLE_DEB
 export const SSL_CERT = env.SSL_CERT ?? path.resolve(__dirname, 'resources', 'DigiCertGlobalRootG2.crt.pem')
 
 export const REDUNDANT_TOKEN_CRON = env.REDUNDANT_TOKEN_CRON ?? '0 1 * * 0'
-export const DUPLICATE_TOKEN_CRON = env.DUPLICATE_TOKEN_CRON ?? '*/15 * * * *'
+export const REDUNDANT_TOKEN_RUN_ON_STARUP: boolean = JSON.parse(env.REDUNDANT_TOKEN_RUN_ON_STARUP ?? 'false')
 export const DELETE_TOKEN_BATCH_SIZE = parseInt(env.DELETE_TOKEN_BATCH_SIZE ?? '1000')
+
+export const DUPLICATE_TOKEN_CRON = env.DUPLICATE_TOKEN_CRON ?? '*/15 * * * *'
+export const DUPLICATE_TOKEN_RUN_ON_STARTUP: boolean = JSON.parse(env.DUPLICATE_TOKEN_RUN_ON_STARTUP ?? 'false')
 
 export const SLACK_API_TOKEN = env.SLACK_API_TOKEN
 export const SLACK_CHANNEL_NOTIFICATION_ID = env.SLACK_CHANNEL_NOTIFICATION_ID

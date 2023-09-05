100ddf70029bd7032a1db2e8ceedccd2cedcbc54
jaylco <77386599+jaylco@users.noreply.github.com>
2023-09-05 13:52:52 +0100

    csv processor

--- src/service/file/csv.ts
+++ src/service/file/csv.ts
@@ -1,10 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-types
-export const objsToCsv = (objects: any[]): string => {
-  const csv = Object.keys(objects[0]).join(',') + '\n'
-  return csv + objects.map(c => {
-    return Object.values(c).map(c => {
-      const value = c as string
-      return `"${value}"`
-    })
-  }).join('\n')
+import ObjectsToCsv from 'objects-to-csv'
+
+export const objsToCsv = async (objects: any[]): Promise<string> => {
+  const csv = new ObjectsToCsv(objects)
+  return await csv.toString(true)
 }

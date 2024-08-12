3cf8e9b351a47ffd08a70b50cf6c5263f7571897
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-08-12 14:33:27 +0100

    Notification config

--- src/config.ts
+++ src/config.ts
@@ -66,7 +66,7 @@ const config = {
     }
   },
   azure: {
-    siteName: env.WEBSITE_SITE_NAME ?? 'csl-jobs',
+    siteName: env.WEBSITE_SITE_NAME ?? 'csl-jobs-local',
     storage: {
       accountConnectionString: env.WEBSITE_CONTENTAZUREFILECONNECTIONSTRING ?? 'AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;',
       table: {

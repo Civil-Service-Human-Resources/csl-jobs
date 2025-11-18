0f8c14447095f9bc8e9ef061ab53dd765ff1ae05
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2025-11-18 09:16:12 +0000

    LC-3927-Update Skills CSL learner record automated job to support pipe delimiter

--- src/service/sftp/service.ts
+++ src/service/sftp/service.ts
@@ -2,24 +2,24 @@ import { createSftpConnection } from './connection'
 import config from '../../config'
 import log from 'log'
 
-export const uploadToSftp = async (localFilePath: string, csvFileName: string, sshPrivateKey: string | undefined): Promise<boolean> => {
+export const uploadToSftp = async (localFilePath: string, dataFileName: string, sshPrivateKey: string | undefined): Promise<boolean> => {
   const sftp = await createSftpConnection(sshPrivateKey)
   if (sftp !== undefined) {
     const remoteDirectory = config.sftp.skillsSftpRemoteDir
-    const remoteFilePath = `${remoteDirectory}/${csvFileName}`
+    const remoteFilePath = `${remoteDirectory}/${dataFileName}`
     try {
-      log.info(`Uploading ${csvFileName} to sftp remoteFilePath: ${remoteFilePath}...`)
+      log.info(`Uploading data file ${dataFileName} to sftp remote file path: ${remoteFilePath}...`)
       await sftp.put(localFilePath, remoteFilePath)
-      log.info(`File uploaded to sftp remoteFilePath: ${remoteFilePath}`)
+      log.info(`Data file uploaded to sftp remote file path: ${remoteFilePath}`)
       return true
     } catch (err) {
-      log.error('Error uploading file to sftp:', err)
+      log.error('Error uploading data file to sftp:', err)
       return false
     } finally {
       await sftp.end()
     }
   } else {
-    log.info('Either sftp details are not provided or unable to connect to sftp server, therefore csv file is not uploaded.')
+    log.info('Either sftp details are not provided or unable to connect to sftp server, therefore data file is not uploaded.')
     return false
   }
 }

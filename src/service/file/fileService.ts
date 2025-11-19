4d330673750b50137973e470386787d63ae7e3be
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2025-11-19 11:59:39 +0000

    LC-3929 - aikido recommendation applied
    Signed-off-by: Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>

--- src/service/file/fileService.ts
+++ src/service/file/fileService.ts
@@ -10,6 +10,8 @@ export const unlink = async (path: string): Promise<void> => {
 }
 
 export function validateBaseDirAndFileName (baseDir: string, fileName: string): string {
+  validateFileName(fileName)
+
   if (baseDir.includes('..')) {
     throw new Error(`Invalid directory: ${baseDir}`)
   }
@@ -21,8 +23,6 @@ export function validateBaseDirAndFileName (baseDir: string, fileName: string): 
   if (!resolvedPath.startsWith(path.resolve(baseDir))) {
     throw new Error(`Blocked path traversal attempt: ${fileName}`)
   }
-
-  validateFileName(fileName)
 
   return resolvedPath
 }

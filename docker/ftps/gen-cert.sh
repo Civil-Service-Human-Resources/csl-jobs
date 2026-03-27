bc5529c98a66249d783c2b10a9da5e480c396d88
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-03-27 13:34:38 +0000

    Revert gen-cert.sh

--- docker/ftps/gen-cert.sh
+++ docker/ftps/gen-cert.sh
@@ -1,5 +1,8 @@
-mkdir -p certs && cd certs || exit
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/key.pem -out /etc/ssl/private/cert.pem -subj "/C=UK/ST=State/L=London/O=CabinetOffice/CN=localhost" || exit
+mkdir certs && cd certs || exit
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/ssl/private/key.pem \
+  -out /etc/ssl/private/cert.pem \
+  -subj "/C=UK/ST=State/L=London/O=CabinetOffice/CN=localhost" || exit
 
 chmod 600 /etc/ssl/private/cert.pem
 chmod 600 /etc/ssl/private/key.pem
